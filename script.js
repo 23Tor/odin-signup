3f0add8693ac1e362f86bebca01c9b213c96d595
23Tor <trmorton505@gmail.com>
2023-08-17 14:17:35 -0400

    Password format validation

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 // Instantiate objects
 const password = document.getElementById("psw")
 const passwordConfirmation = document.getElementById("passwordconf")
+const passwordPattern = new RegExp("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{8,}")
 const submitButton = document.getElementById("submit-button")
 
 var checkMatch = function () {
   if (document.getElementById('psw').value ==
     document.getElementById('passwordconf').value) {
     document.getElementById('confirmation-message').style.color = 'green';
-    document.getElementById('confirmation-message').innerHTML = 'Matching';
+    document.getElementById('confirmation-message').innerHTML = 'Matching!';
   } else {
     document.getElementById('confirmation-message').style.color = 'red';
     document.getElementById('confirmation-message').innerHTML = 'Not matching';
@@ -15,5 +16,11 @@ var checkMatch = function () {
 }
 
 var checkFormat = function () {
-  
-}
+  if (passwordPattern.test(password.value)) {
+    document.getElementById("password-message").style.color = 'green';
+    document.getElementById("password-message").innerHTML = 'Valid!';
+  } else {
+    document.getElementById("password-message").style.color = 'red';
+    document.getElementById("password-message").innerHTML = 'Invalid';
+  }
+};
